62fe54979a389940fe9813b5bc846cdd885a4fe7
space8033 <space8237@naver.com>
2023-10-11 15:31:54 +0900

    feat : 템플릿 적용 완료

--- src/main/java/com/finalteam3/exodia/Main.java
+++ src/main/java/com/finalteam3/exodia/Main.java
@@ -1,13 +1,14 @@
 package com.finalteam3.exodia;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Main {
 	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
 		return "index";
 	}
 	
