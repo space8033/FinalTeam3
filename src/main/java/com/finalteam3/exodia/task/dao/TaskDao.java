81dc3070d7d18ee50371c956740ea49194fb7397
space8033 <space8237@naver.com>
2023-11-07 00:37:33 +0900

    feat : main페이지 인원별 진척률 완료

--- src/main/java/com/finalteam3/exodia/task/dao/TaskDao.java
+++ src/main/java/com/finalteam3/exodia/task/dao/TaskDao.java
@@ -1,12 +1,14 @@
 package com.finalteam3.exodia.task.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.finalteam3.exodia.task.dto.request.ProgramModifyRequest;
 import com.finalteam3.exodia.task.dto.request.ProgramRegisterRequest;
 import com.finalteam3.exodia.task.dto.response.ProgramListResponse;
+import com.finalteam3.exodia.task.dto.response.TeamTaskResponse;
 
 @Mapper
 public interface TaskDao {
@@ -17,4 +19,7 @@ public interface TaskDao {
 	public void deleteProgram(int task_no);
 	public int countAllTask(int project_no);
 	public int countCompleteTask(int project_no);
+	public int countTaskByEmpNo(Map<String, Object> map);
+	public int countCompleteByEmpNo(Map<String, Object> map);
+	public List<TeamTaskResponse> selectTeamTaskDetail(Map<String, Object> map);
 }
