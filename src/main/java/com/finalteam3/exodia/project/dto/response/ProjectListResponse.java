3e1c7f70cc8e75b46f1e5203d86d47baf88bdd05
space8033 <space8237@naver.com>
2023-11-09 09:09:00 +0900

    chore

--- src/main/java/com/finalteam3/exodia/project/dto/response/ProjectListResponse.java
+++ src/main/java/com/finalteam3/exodia/project/dto/response/ProjectListResponse.java
@@ -6,4 +6,9 @@ import lombok.Data;
 public class ProjectListResponse {
 	private int project_no;
 	private String project_name;
+	private int project_memberCount;
+	private String project_startdate;
+	private String project_enddate;
+	private String project_period;
+	private String project_client;
 }
