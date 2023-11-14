be288b4e8ca4a68385fcad1b1719ff63ca571938
space8033 <space8237@naver.com>
2023-11-14 11:08:03 +0900

    chore : 색상 순서 수정

--- src/main/webapp/resources/assets/js/app-user-list.js
+++ src/main/webapp/resources/assets/js/app-user-list.js
@@ -80,8 +80,10 @@ $(function () {
 	            '<img src="data:MIME;base64, ' + $image + '" alt="Avatar" class="rounded-circle">';
 	        } else {
 	            // For Avatar badge
-	          var stateNum = Math.floor(Math.random() * 6);
-	          var states = ['success', 'danger', 'warning', 'info', 'dark', 'primary', 'secondary'];
+	          console.log(full['emp_no']);
+	          var stateNum = full['emp_no'] % 7;
+	          console.log(stateNum);
+	          var states = ['success', 'primary', 'warning', 'danger', 'info', 'dark', 'secondary'];
 	          var $state = states[stateNum],
 	          $name = full['empinfo_name'],
 	          $initials = $name.slice(-2);
@@ -105,18 +107,7 @@ $(function () {
                 '</div>' +
                 '</div>';
               return $row_output;
-            /*var $row_output =
-              '<div class="d-flex justify-content-start align-items-center user-name">' +
-              '<div class="d-flex flex-column">' +
-              '<span class="fw-medium">' +
-              $name +
-              '</span>' +
-              '<small class="text-muted">' +
-              $email +
-              '</small>' +
-              '</div>' +
-              '</div>';
-            return $row_output;*/
+
           }
         },
         {
