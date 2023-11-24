a9a011355a1e6f0579c2fc6501713745bf8bf3e7
space8033 <space8237@naver.com>
2023-11-24 13:51:23 +0900

    chore

--- src/main/webapp/resources/assets/js/main.js
+++ src/main/webapp/resources/assets/js/main.js
@@ -22,7 +22,7 @@
 	    	  var emp_id = $("#alarmId").val();
 	    	  console.log(emp_id+"야 잘보내긴해?");
 	         sock.send(emp_id);
-	      }, 1000);
+	      }, 500);
     }
 
 	function handleWebSocketMessage(message) {
